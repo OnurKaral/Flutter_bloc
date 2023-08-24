050082a3a435c141c177c409b0ee6e08b4429024
OnurKaral <santobixua@gmail.com>
2023-08-24 17:01:33 +0300

    entity fixed

--- lib/src/data/remote/api_service.dart
+++ lib/src/data/remote/api_service.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:studio_flutter/src/core/utils/constants.dart';
 import 'package:studio_flutter/src/data/model/player_response/player_response.dart';
 import 'package:studio_flutter/src/injector.dart';
-import 'package:studio_flutter/src/utils/constants.dart';
 
 class ApiService {
   final client = injector<Dio>();
